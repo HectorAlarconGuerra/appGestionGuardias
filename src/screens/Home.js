c6fd443e42fb84b563d8f1d54c1adb78a2c5a551
hectoralarcon <hectoralarconguerra@gmail.com>
2020-12-20 09:07:36 -0500

    Hace la peticion y devuelve las peliculas

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, Button, Image, StyleSheet} from 'react-native';
+import {getNewsMoviesApi} from '../api/movies';
 
 export default function Home(props) {
   const {navigation} = props;
+  const [newMovies, setNewMovies] = useState(null);
+  console.log(newMovies);
+
+  useEffect(() => {
+    getNewsMoviesApi().then((response) => {
+      setNewMovies(response.results);
+    });
+  }, []);
+
   return (
     <View>
       <Text>Estamos en Home</Text>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
+export const API_HOST = 'https://api.themoviedb.org/3';
 export const API_KEY = '5cdcfed2516efc37c7cc85f4fb7fe711';
+export const LANG = 'es-ES';
