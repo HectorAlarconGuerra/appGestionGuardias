59eb70b18e53caffef83093ee38237562a67c907
HectorAlarconGuerra <33630876+HectorAlarconGuerra@users.noreply.github.com>
2020-12-28 15:56:01 -0500

    Carusel pinta peliculas

--- src/components/CarouselVertical.js
+++ src/components/CarouselVertical.js
@@ -8,7 +8,7 @@ import {
 } from 'react-native';
 import {Text, Title} from 'react-native-paper';
 import Carousel from 'react-native-snap-carousel';
-
+import {BASE_PATH_IMG} from "../utils/constants";
 const {width} = Dimensions.get('window');
 const ITEM_WIDTH = Math.round(width * 0.7);
 
@@ -27,12 +27,17 @@ export default function CarouselVertical(props) {
 
 function RenderItem(props) {
   const {data} = props;
-  const {title} = data.item;
+  const {title, poster_path} = data.item;
+  const imageUrl = `${BASE_PATH_IMG}/w500${poster_path}`
+   
+
+  console.log(imageUrl);
 
   return (
     <TouchableWithoutFeedback onPress={() => console.log('HOLA')}>
       <View style={StyleSheet.card}>
-        <Image></Image>
+       <Image style={styles.image} source={{uri: imageUrl}}/>
+       <Title style={styles.title}>{title}</Title>
       </View>
     </TouchableWithoutFeedback>
   );
@@ -48,4 +53,13 @@ const styles = StyleSheet.create({
     shadowOpacity: 1,
     shadowRadius: 10,
   },
+  image:{
+    width: "100%",
+    height: 450,
+    borderRadius: 20
+  },
+  title: {
+    marginHorizontal: 10,
+    marginTop:10
+  }
 });
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
 export const API_HOST = 'https://api.themoviedb.org/3';
 export const API_KEY = '5cdcfed2516efc37c7cc85f4fb7fe711';
 export const LANG = 'es-ES';
+export const BASE_PATH_IMG = "https://image.tmdb.org/t/p";
